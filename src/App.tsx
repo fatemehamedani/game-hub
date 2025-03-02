646f03528a3a960bee800bf461cdee09cf80b063
fatemehamedani <ftmehmdni1380@gmail.com>
2025-03-02 23:29:31 +0330

    Project: Building a Video Game  Discovery App / Building the Navigation Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import "./App.css";
-import React from "react";
+import NavBar from "./components/NavBar";
 
 const App = () => {
-  return <div className="bg-black">App</div>;
+  return (
+    <div
+      className="grid w-screen
+    "
+      style={{
+        gridTemplateAreas: `"nav nav" "aside main"`,
+        gridTemplateColumns: "1fr 1fr",
+        gridTemplateRows: "auto 2fr",
+      }}
+    >
+      <div className="bg-amber-600 py-2" style={{ gridArea: "nav" }}>
+        <NavBar />
+      </div>
+      <div
+        className="bg-amber-400 py-2 flex justify-center"
+        style={{ gridArea: "aside" }}
+      >
+        Aside
+      </div>
+      <div
+        className="bg-blue-600 py-2 flex justify-center"
+        style={{ gridArea: "main" }}
+      >
+        Main
+      </div>
+    </div>
+  );
 };
 
 export default App;
