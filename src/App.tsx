9ccafc5f717953934681e6cb743a56109cd987ec
fatemehamedani <ftmehmdni1380@gmail.com>
2025-03-23 22:13:00 +0330

    Fetch the genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 import NavBar from "./components/NavBar";
 // import Theme from "./Theme";
 
@@ -21,7 +22,7 @@ const App = () => {
         {/* <Theme /> */}
       </div>
       <div className="py-2 flex justify-center" style={{ gridArea: "aside" }}>
-        Aside
+        <GenreList />
       </div>
       <div
         className="py-2 w-full flex justify-center"
